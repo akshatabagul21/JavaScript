0e1e46952a56460e822a72965120ab51a27991ba
abagul <akshata.bagul@radisys.com>
2025-01-08 10:10:53 +0530

    initial commit

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert("hello"); we are using node js not browser
+
+// console.log(); console.log()
+console.log(typeof null);
+console.log(typeof undefined);    // null is an object , but undefined apne aap me hi ek type h of undefined , & called as language error   ********interview Q.***
+//***********************primitive data Types*************************************** */
+let name = "hitesh" //string
+
+let age = 18 //number  number range = > 2 to power 53
+//bigint for v. big number
+
+let isLoggedIn = false //boolean
+
+//null (standalone value,it is a type) undefined nahi h , but voh khali h
+
+//undefined (variable declared but not defined, value assign nahi huyee hai)
+
+//symbol (unique)
+
+
+
+//object
